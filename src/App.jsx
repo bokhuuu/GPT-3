a29e94680497b815c78360ef64ef9c97b9f89f66
bokhuuu <zurabokhua@gmail.com>
2023-08-02 15:29:38 +0400

    established css files for project,with setup main css styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 
 import { Header, WhatGPT3, Features, Possibility, Blog, Footer } from "./containers"
 import { Navbar, Brand, CTA } from "./components"
+import "./App.css"
 
 export default function App() {
     return (
         <div className='app'>
-            <div className='gradient_bg'>
+            <div className='gradient__bg'>
                 <Navbar />
                 <Header />
             </div>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import { useState } from 'react'
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri"
+import logo from "../../assets/logo.svg"
 import "./navbar.css"
 
 export default function Navbar() {
+    const [toogleMenu, setToogleMenu] = useState(false)
+
     return (
-        <div>Navbar</div>
+        <div className='gpt3__navbar'>
+            <div className='gpt3__navbar-links'>
+                <div className='gpt3__navbar-links_logo'>
+                    <img src={logo} alt='logo' />
+                </div>
+                <div className='gpt3__navbar-links_container'>
+                    <p><a href="#home">Home</a></p>
+                    <p><a href="#wgpt3">What is GPT?</a></p>
+                    <p><a href="#possibility">Open AI</a></p>
+                    <p><a href="features">Case Studies</a></p>
+                    <p><a href="#blog">Library</a></p>
+                </div>
+                <div className='gpt3__navbar-sign p'>
+                    <p>sign in</p>
+                    <button type="button">Sign Up</button>
+                </div>
+                {/* <div className='gpt3__navbar-menu'>
+                    {toogleMenu ? <RiCloseLine color="white" size={27}
+                        onClick={() => setToogleMenu(false)} />
+                        : <RiMenu3Line color="white" size={27}
+                            onClick={() => setToogleMenu(true)} />
+                    }
+                </div> */}
+            </div>
+        </div>
     )
 }
