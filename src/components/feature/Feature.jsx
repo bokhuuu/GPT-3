37fe779dbae058a137e7b3d3b25a2a2c10379798
bokhuuu <zurabokhua@gmail.com>
2023-08-04 14:54:12 +0400

    implemented features componenet,wich is generated via map method

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -3,15 +3,15 @@ import "./feature.css"
 
 
 const Feature = ({ title, text }) => (
-    <div className="gpt3__features-container-feature">
-      <div className="gpt3__features-container-feature-title">
-        <div />
-        <h1>{title}</h1>
-      </div>
-      <div className="gpt3__features-container-feature-text">
-        <p>{text}</p>
-      </div>
+  <div className="gpt3__features-container-feature">
+    <div className="gpt3__features-container-feature-title">
+      <div />
+      <h1>{title}</h1>
     </div>
-  );
-  
-  export default Feature;
+    <div className="gpt3__features-container-feature-text">
+      <p>{text}</p>
+    </div>
+  </div>
+);
+
+export default Feature;
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -4,7 +4,7 @@ import Feature from '../../components/feature/Feature'
 
 export default function WhatGPT3() {
     return (
-        <div className='gpt3__whatgpt section__margin id="wgpt3'>
+        <div className="gpt3__whatgpt section__margin" id="wgpt3">
             <div className='gpt3__whatgpt3-feature'>
                 <Feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by." />
             </div>
@@ -15,9 +15,9 @@ export default function WhatGPT3() {
                 <p>Explore the Library</p>
             </div>
             <div className='gpt3__whatgpt3-container'>
-            <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought." />
-            <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
-            <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
+                <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought." />
+                <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
+                <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
             </div>
 
         </div >
