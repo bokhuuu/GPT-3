0ea3f227d930b5e740c0d53639ee465f36fccdea
bokhuuu <zurabokhua@gmail.com>
2023-08-04 12:51:57 +0400

    implemented feature component,wich is reusable via props

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,17 +1,9 @@
 import React from "react"
 import "./features.css"
 
-export default function Features({ title, text }) {
+
+export default function Features() {
     return (
-        <div className='gpt3__features-container-feature'>
-            <div className='gpt3__features-container-feature-title'>
-                <div>
-                    <h1>{title}</h1>
-                </div>
-                <div className='gpt3__features-container-feature-text'>
-                    {text}
-                </div>
-            </div>
-        </div>
+        <div>features</div>
     )
 }
